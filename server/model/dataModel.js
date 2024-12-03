c35fdf093c76b9eecf25362155e0f8dd2c0b370b
David Kopl - PC <kopldavid@seznam.cz>
2024-12-03 16:44:33 +0100

    added validity

--- server/model/dataModel.js
+++ server/model/dataModel.js
@@ -2,9 +2,30 @@ const mongoose = require('mongoose');
 
 const dataSchema = new mongoose.Schema({
   sensor_id: String,
+  timestamp: { type: Date, default: Date.now },
   temperature: Number,
   humidity: Number,
-  timestamp: { type: Date, default: Date.now },
+  co2: { type: Number, default: null },
+  ec: { type: Number, default: null },
+  ph: { type: Number, default: null },
+  do: { type: Number, default: null },
+  adc_readings: {
+    adc0_EC: { type: Number, default: null },
+    adc1_Ph: { type: Number, default: null },
+    adc2_DO: { type: Number, default: null },
+  },
+  relays: {
+    relay1_hum_minus: { type: Boolean, default: false },
+    relay2_temp_minus: { type: Boolean, default: false },
+    relay3_temp_plus: { type: Boolean, default: false },
+    relay4_co2_plus: { type: Boolean, default: false },
+  },
+  errors: {
+    temp_hum_err: { type: Boolean, default: false },
+    EC_error: { type: Boolean, default: false },
+    Ph_error: { type: Boolean, default: false },
+    DO_error: { type: Boolean, default: false },
+  },
 });
 
 const Data = mongoose.model('Data', dataSchema);
--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
+const validateData = require('./middleware/validateData');
 const port = 3000;
 
 // Middleware pro zpracování JSON požadavků
 app.use(express.json());
 
 // Endpoint pro příjem dat
-app.post('/data', (req, res) => {
+app.post('/data', validateData, (req, res) => {
   console.log('Received data:', req.body);
-  res.status(200).send('Data received');
+  res.status(200).send('Data received and valid!');
 });
 
 // Spuštění serveru
