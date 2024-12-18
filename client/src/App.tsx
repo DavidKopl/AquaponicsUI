099f479d0039c71d6d1aa36d059c8082f38182a8
David Kopl - PC <kopldavid@seznam.cz>
2024-12-18 18:56:39 +0100

    complete all

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -142,9 +142,11 @@ function App() {
         <MyLineChart selectedParameter="co2" label={'CO2'} unit={'ppm'} color={'#f28e2c'} />
         <MyLineChart selectedParameter="current_leaf_VPD" label={'VPD (list)'} unit={'(kPa)'} color={'green'} />
         <MyLineChart selectedParameter="do" label={'DO'} unit={'ppm'} color={'blue'} />
-        <div>
-          <p>Poslední naměřená hodnota EC: {ec != null ? ec.toFixed(2) : 'N/A'}</p>
-          <p>Poslední naměřená hodnota pH: {ph != null ? ph.toFixed(2) : 'N/A'}</p>
+        <div className="flex justify-center items-center">
+          <div className="text-center">
+            <p className="mb-4 text-4xl text-gray-500 dark:text-gray-800">Aktuální EC: {ec != null && ec > 0.3 ? ec.toFixed(2) : 'Senzor není připojen'}</p>
+            <p className="mb-4 text-4xl text-gray-500 dark:text-gray-800">Aktuální pH: {ph != null && ph >= 3 && ph <= 8 ? ph.toFixed(2) : 'Senzor není připojen'}</p>
+          </div>
         </div>
       </div>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-4">
@@ -153,8 +155,8 @@ function App() {
         <ArcDesign value={co2} min={400} max={1600} opt={1000} name="CO2" />
         {/* <ArcDesign value={dissolvedOxygen} min={0} max={20} opt={8} name="Dissolved Oxygen" /> //TODO: Zobrazovat jako graf*/}
         <ArcDesign value={vpd['current_leaf_VPD']} min={0.8} max={1.7} opt={1.2} name="VPD" />
-        <ArcDesign value={ec} min={0} max={5} opt={2} name="EC" />
-        <ArcDesign value={ph} min={0} max={14} opt={6.5} name="pH" />
+        {/* <ArcDesign value={ec} min={0} max={5} opt={2} name="EC" />
+        <ArcDesign value={ph} min={0} max={14} opt={6.5} name="pH" /> */}
       </div>
     </>
   );
